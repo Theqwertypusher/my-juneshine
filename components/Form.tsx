5d6869abc26390334a35dd43c8ba3d8aebda8287
jason-victor <jason.victor@talkspace.com>
2024-01-01 11:37:02 -0500

    draft update

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,22 +1,35 @@
 import { useForm } from "react-hook-form";
 import { joiResolver } from "@hookform/resolvers/joi";
 import Joi from "joi";
+import { useEffect } from "react";
 
 const schema = Joi.object({
-  name: Joi.required().valid("sunny"),
+  name: Joi.string().required().valid("sunny"),
 });
 
 export const Form = () => {
   const {
     register,
     handleSubmit,
+    setError,
     watch,
     formState: { errors },
   } = useForm({
+    criteriaMode: "all",
     resolver: joiResolver(schema),
   });
 
   const onSubmit = (e: any) => console.log(e);
+
+  useEffect(() => {
+    setError("name", {
+      types: {
+        required: "This is required",
+        minLength: "This is minLength",
+      },
+    });
+    console.log("errors", errors);
+  }, [setError, errors]);
 
   return (
     <form className="w-full max-w-sm" onSubmit={handleSubmit(onSubmit)}>
@@ -24,7 +37,6 @@ export const Form = () => {
         <input
           className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
           type="text"
-          placeholder="Jane Doe"
           aria-label="Full name"
           {...register("name")}
         />
--- components/Typer.tsx
+++ components/Typer.tsx
@@ -11,21 +11,29 @@ export const Typer: FC<TyperProps> = ({ onComplete }) => {
   // Create reference to store the DOM element containing the animation
   const el = React.useRef(null);
 
+  const inConstructionMessage = [
+    "Hello",
+    "did you find this site by mistake?",
+    "If yes, ignore",
+    "Otherwise, stay tuned and do not tell anyone",
+    "namely, Jay",
+  ];
+
   const greetingMessage = [
     "Why hello onion bat.",
-    // "Before I type on,",
-    // "whatever you do, do not tell me (Jay) that you found this",
-    // "And to make sure I know it’s you. Answer this question!",
-    // "What’s the name of the cat who hates you and all other Jess’s in this world?",
+    "Before I type on,",
+    "whatever you do, do not tell me (Jay) that you found this",
+    "And to make sure I know it’s you. Answer this question!",
+    "What’s the name of the cat who hates you and all other Jess’s in this world?",
   ];
 
   React.useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: greetingMessage,
+      strings: inConstructionMessage,
       typeSpeed: 50,
-      onComplete: () => {
-        console.log("complete"), onComplete();
-      },
+      // onComplete: () => {
+      //   console.log("complete"), onComplete();
+      // },
     });
 
     return () => {
